994aa7144fc849ed0b68711a177803a27ae73e26
Rupani Bhardwaj <raabhardwaj@gmail.com>
2022-02-08 19:38:20 +0530

    All Excel Sheet add Program

--- Project/todayproject.py
+++ Project/todayproject.py
@@ -1,4 +1,6 @@
-import openpyxl
+# import openpyxl
+import pandas as pd
+
 '''
 df1 = openpyxl.load_workbook("OfficeM&A_RootWords.xlsx")
 print(type(df1))
@@ -9,19 +11,34 @@ s = sheet['A12'].value
 print(s)
 print(sheet.cell(14, 4).value)'''
 
-# find the All data excel particular sheet file
-df2 = openpyxl.load_workbook("OfficeM&A_RootWords.xlsx")
-sheet1 = df2['RootWords']
+# find The All data excel particular sheet file
+
+# df2 = openpyxl.load_workbook("OfficeM&A_RootWords.xlsx")
+'''sheet1 = df2['RootWords']
 row = sheet1.max_row
 column = sheet1.max_column
 x3 = sheet1.cell(row=3, column=1)
 #print(x3.value)
 for i in range(1, row+1):
     for j in range(1, column):
-        print(sheet1.cell(i, j).value)
+        print(sheet1.cell(i, j).value)'''
 
-#find the All data excel particular sheet file in column and row
+df = pd.read_excel("OfficeM&A_RootWords.xlsx")
+df1 = pd.read_excel("OfficeM&A_RootWords.xlsx", sheet_name='RootWords', usecols="B")
+print(df1)
+'''df.to_excel("OfficeM&A_RootWords.xlsx", engine='xlsxwriter')
 
+# Open it with xlsxwriter
+writer = pd.ExcelWriter("OfficeM&A_RootWords.xlsx", engine='xlsxwriter')
+df.to_excel(writer, sheet_name='RootWords')
 
+# Assign the workbook and worksheet
+workbook = writer.book
+worksheet = writer.sheets['RootWords']
 
+# Adding the header and Datavalidation list
+worksheet.write('D1', 'Status')
+worksheet.data_validation('D2', {'validate': 'list',
+                                 'source': ['Approved', ' Rejected', 'Partially Approved']})
 
+workbook.close()'''
